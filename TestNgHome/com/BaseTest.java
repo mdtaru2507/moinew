d5b10ddc4ee68a5df0f78dd453ed4c1de18f77b3
MD MOIN UDDIN <manhamahdi@mds-macbook-pro.mynetworksettings.com>
2023-10-15 15:31:57 -0400

    add project

---
+++ TestNgHome/com/BaseTest.java
@@ -0,0 +1,65 @@
+package com;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class BaseTest {
+	
+  @Test(dataProvider = "dp")
+  public void f(Integer n, String s) {
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("Hello Hello Hello");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "a" },
+      new Object[] { 2, "b" },
+    };
+  }
+  
+  @BeforeClass
+  public void beforeClass() {
+	  
+	  System.out.println("Hello3 Hello 3 Hello");
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+	  
+	  System.out.println("Hello 2 Hello 2");
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
